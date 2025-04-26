26eedd59dce5bab372774227e0c4d60c3fef81da
andreishark <andreishark10@gmail.com>
2025-04-26 17:54:47 +0300

    Code complete

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import grpc
 from chatbot.chatbot import get_response
 from models.chatbot_response import ChatbotResponse
