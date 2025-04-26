2aaf9d803d7f912561b6ff9fa9bd5a01566c50fd
andreishark <andreishark10@gmail.com>
2025-04-26 16:40:18 +0300

    Complete code

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -53,6 +53,6 @@ def check_all_messages(message):
 
 
 def get_response(user_input):
-    split_message = re.split(r"\s+|[,;?-]\s", user_input.lower())
+    split_message = re.split(r"\s+|[,;?-]*\s", user_input.lower())
     response = check_all_messages(split_message)
     return response
